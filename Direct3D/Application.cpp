be89f8fcf1d0166d108b167ee5dc2730dda1e4aa
PC88 <32823980+PC88@users.noreply.github.com>
2019-12-22 18:20:12 +0000

    basic window registration and descriptor.

--- Direct3D/Application.cpp
+++ Direct3D/Application.cpp
@@ -1,6 +1,36 @@
 #include "iostream"
+#include "windows.h"
 
-int main()
+int CALLBACK WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
 {
+	const auto pClassName = "InitialClass3D";
+
+	// register window class: set up descriptor
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInst;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+
+	RegisterClassEx( &wc );
+
+
+	// create window instance
+	HWND hWnd = CreateWindowEx(0, pClassName, "Window", WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, 200,200,640,480,nullptr, nullptr, hInst, nullptr);
+
+	// display window
+	ShowWindow(hWnd, SW_SHOW);
+	while (true)
+	{
+		std::cin.get();
+	}
 	return 0;
 }
